a9ea969a857155ee517752ea1e5d496885cdee07
agustindiazcano <agustin.td@gmail.com>
2021-04-09 12:38:51 -0300

    compatibilidad node

--- back/utils/differenceBetweenDates.js
+++ back/utils/differenceBetweenDates.js
@@ -12,7 +12,7 @@
 
 const differenceBetweenDates = (date1, date2, daysToMade = 0) => {
   if (!date2) return 0;
- return daysToMade - (Number(date1.replaceAll('-','')) - Number(date2.replaceAll('-','')));
+  return daysToMade - (Number(date1.replace(/-/g,'')) - Number(date2.replace(/-/g,'')));;
 }
 
 module.exports = differenceBetweenDates;
